61faea8754af2d8d42d81c2946ec1dadee0485a4
KC Santosh <ext_santosh.kc@falabella.cl>
2025-09-05 19:42:52 +0530

    Changes

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -5,7 +5,10 @@ import { ButtonProps } from "@generic/components/Button/Button.types";
 import ButtonMui from "@mui/material/Button";
 
 const Button: React.FC<ButtonProps> = ({ children, ...props }) => (
-    <ButtonMui {...props}>{children}</ButtonMui>
+    <>
+        KC
+        <ButtonMui {...props}>{children}</ButtonMui>
+    </>
 );
 
 export default Button;

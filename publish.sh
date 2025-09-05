a6264014679bc5abb27f00d6df1c3cf5c392a408
KC Santosh <ext_santosh.kc@falabella.cl>
2025-09-05 08:51:34 +0530

    Auto publishing

---
+++ publish.sh
@@ -0,0 +1,11 @@
+npm version patch
+
+npm i
+
+git add package.json package-lock.json
+
+VERSION=$(node -p "require('./package.json').version")
+
+git commit -m "chore: release v$VERSION"
+
+git push origin dev
